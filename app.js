fdf4ab9ed3c1c88aa48dc8719a9c9ca55572b171
Spaceaadity <aadi.nickels@gmail.com>
2016-01-29 06:40:08 -0800

    this was mostly trying to figure out how to display social media and query apis

--- app.js
+++ app.js
@@ -1,71 +1,33 @@
-var http = require('http');
-
-var fs = require('fs');
-
-var server = http.createServer(function (request, response){
-	console.log('client request URL: ', request.url);
-	if(request.url ==='/') {
-		fs.readFile('index.html', 'utf8', function (errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write(contents);
-			response.end();
-		});
-	} else if(request.url === '/css/bootstrap.min.css'){
-		fs.readFile('./css/bootstrap.min.css', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/css'});
-			response.write(contents);
-			response.end();
-		});
-	}else if(request.url === '/css/agency.css'){
-		fs.readFile('./css/agency.css', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/css'});
-			response.write(contents);
-			response.end();
-		});
-	} else if(request.url === '/font-awesome/css/font-awesome.min.css'){
-		fs.readFile('./font-awesome/css/font-awesome.min.css', 'utf8', function(errors, contents) {
-			response.writeHead(200, {'Content-Type': 'text/css'});
-			response.write(contents);
-			response.end();
-		});
-	}else if(request.url === 'js/jquery.js'){
-		fs.readFile('./js/jquery.js', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/javascript'});
-			response.write(contents);
-			response.end();
-		});
-	}else if(request.url === '/js/bootstrap.min.js'){
-		fs.readFile('./js/bootstrap.min.js', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/javascript'});
-			response.write(contents);
-			response.end();
-		});
-	}else if(request.url === '/js/classie.js'){
-		fs.readFile('./js/classie.js', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/javascript'});
-			response.write(contents);
-			response.end();
-		});
-	}else if(request.url === '/js/cbpAnimatedHeader.js'){
-		fs.readFile('./js/cbpAnimatedHeader.js', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/javascript'});
-			response.write(contents);
-			response.end();
-		});
-	}else if(request.url === '/js/jqBootstrapValidation.js'){
-		fs.readFile('./js/jqBootstrapValidation.js', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/javascript'});
-			response.write(contents);
-			respone.end
-		})
-	}
-
-	else {
-		response.writeHead(404);
-		response.end('File not found!!');
-	}
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+var Twitter = require('twitter');
+var tweet = new Twitter({
+  consumer_key: 'qxSlECFZlWgOibzyrHuKlCyAa',
+  consumer_secret: 'fiHsc3CJlm7pdmJWxBkjM9AWMTiuvnwvLlwuOBZyhM07vF88Kp',
+  access_token_key: '1321772126-6gV81BPj5pV94vkjxVx79y43hemVcnKSjDYPCUs',
+  access_token_secret: 'PDduBy4IC6vuxCjF23YenUE0HaZwROixqs9wTcm6oy095'
 });
-
-server.listen(6789);
-
-console.log("running in localhost at port 6789");
+tweet.get('statuses/user_timeline', {screen_name: '@spaceaadity', count: 1}, function(error, tweets, response) {
+	if(!error){
+		 console.log(tweets);
+	//not working - how do I get my most recent tweet's id to request the embeded format
+		// var twit = JSON.parse(tweets[0]);
+		// var current = twit.id;
+		// console.log(current);
+		// tweet.get('statuses/oembed', {id: 692789941290414100}, function(response){
+		// 	console.log(response);
+		// });
+	};
+});
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './static')));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.get('/', function(request, response){
+	response.render('index');
+});
+app.listen(8000, function(){
+	console.log("Channel 8000 open for communication Captain.");
+});
